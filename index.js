c566119b921d5bee372a8876e9b7ab62ea986666
Gunjan Kalita <gunjankalita@Gunjans-MacBook-Air.local>
2021-07-03 00:40:01 +0530

    Add middleware to Impress

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
-var Impress = require('./Impress/Impress');
+var Impress = require('./Impress/Impress')
 
-Impress.post('/one/:id/comment/:comments', function(req, res) {
-  res.send(200, {
-    body: req.body,
-    params: req.params,
-  });
-});
 
-Impress.listen(9000, function(message) {
-  console.log(message);
-});
+const auth = (req, res, next) =>{
+    setTimeout(()=>{
+        console.log('auth middleware passed!');
+    next()
+    }, 200) 
+}
+
+const secondaryAuth = (req, res, next) =>{
+   setTimeout(()=>{
+    console.log('sec auth middleware passed!', typeof next);
+    next()    
+   }, 300)
+}
+
+
+
+const TAuth = (req, res, next) =>{
+   setTimeout(()=>{
+    console.log('T auth middleware passed!', typeof next);
+    next()    
+   }, 400)
+}
+
+
+const BAuth = (req, res, next) =>{
+   setTimeout(()=>{
+    console.log('B auth middleware passed!', typeof next);
+    next()    
+   }, 450)
+}
+
+Impress.post('/one', auth, secondaryAuth, TAuth, BAuth,  function (req, res) {
+    res.send(200, "method executed")
+    console.log('one');
+
+})
+
+Impress.listen()
