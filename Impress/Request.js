7069a841f33819816c14c772578a571d528b04cc
Gunjan Kalita <gunjankalita@Gunjans-MacBook-Air.local>
2021-09-07 23:43:51 +0530

    Code cleanup

--- Impress/Request.js
+++ Impress/Request.js
@@ -2,33 +2,34 @@ const Request = (req, route ,cb) => {
   // required URL structure
   // /user/:id/comment/:comment
 
-  let header = req.headers
-  let content_type = ''
-  let boundary = ''
-  if(header["content-type"].indexOf(";") === -1 ){
-      content_type = header["content-type"]
-  }else {
-      content_type = header["content-type"].substring(0,header["content-type"].indexOf(";")); 
-      boundary = header["content-type"].substring(header["content-type"].indexOf(";")+1, header["content-type"].length-1);
-      boundary = boundary.substring(boundary.indexOf("=")+1, boundary.length-1)
-  }
-
-      let bodyStr = ''
-      req.on('data', chunk => {
-          bodyStr += chunk.toString(); 
-      })
-      .on('error', error=>{
-          cb(null, error)
-      })
-      .on('end', () => {
-          if(content_type==="application/json"){
-              const params = parseUrlData(req.url , route)
-               cb({...req , body: JSON.parse(bodyStr) , params})
-          }else if (content_type==="multipart/form-data"){
-              const argArray = body.split(boundary)
-              return extractMultiformData(argArray)
-          }
-      });
+    let header = req.headers
+    let content_type = ''
+    let boundary = ''
+    if (header["content-type"]){
+        if(header["content-type"].indexOf(";") === -1 ){
+            content_type = header["content-type"]
+        }else {
+            content_type = header["content-type"].substring(0,header["content-type"].indexOf(";")); 
+            boundary = header["content-type"].substring(header["content-type"].indexOf(";")+1, header["content-type"].length-1);
+            boundary = boundary.substring(boundary.indexOf("=")+1, boundary.length-1)
+        }
+    }
+    let bodyStr = ''
+    req.on('data', chunk => {
+        bodyStr += chunk.toString(); 
+    })
+    .on('error', error=>{
+        cb(null, error)
+    })
+    .on('end', () => {
+        if(content_type==="application/json"){
+            const params = parseUrlData(req.url , route)
+            cb({...req , body: bodyStr ? JSON.parse(bodyStr) : {} , params})
+        }else if (content_type==="multipart/form-data"){
+            const argArray = body.split(boundary)
+            return extractMultiformData(argArray)
+        }
+    });
 
 
 
@@ -37,7 +38,7 @@ const Request = (req, route ,cb) => {
 const parseUrlData = ( url , route ) =>{
   const routeSplit = route.split('/')
   const urlSplit = url.split('/')
-  let param = {}
+  let params = {}
   if(routeSplit.length !== urlSplit.length){
       throw Error('invalid request')
   }
@@ -45,10 +46,10 @@ const parseUrlData = ( url , route ) =>{
       if(segment.indexOf(':') === 0){
           const key = segment.split(1, segment.length-1)
           const value = urlSplit[index]
-          param[key] = value
+          params[key] = value
       }
   })
-  return param
+  return params
 
 }
 
--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 var Impress = require('./Impress/Impress')
 
+Impress.get('/', (req, res) => {
+    res.send(200, JSON.stringify("Dummy text"))
+})
+
+Impress.get('/def/:ti',(req, res, next) => {
+    req.params.ti = req.params.ti + 'extension';
+    next(); 
+}, (req, res) => {
+    res.send(200, JSON.stringify(req.params))
+})
+
+Impress.listen(3000, ()=>{
+    console.log('server up.');
+})
 module.exports = Impress;
